b4dcc2eaa8bf466681efa2b5349efccf554fbfd6
Prantosh <bepariprantosh360@gmail.com>
2021-11-13 19:51:01 +0600

    update navbar

--- src/pages/Dashboard/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -6,12 +6,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -107,16 +101,7 @@ function Dashboard(props) {
       <Button onClick={logOut} variant="contained">
         Logout
       </Button>
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      
     </div>
   );
 
@@ -225,10 +210,7 @@ function Dashboard(props) {
 }
 
 Dashboard.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
+
   window: PropTypes.func,
 };
 
--- src/pages/Shared/Navigation/Navigation.js
+++ src/pages/Shared/Navigation/Navigation.js
@@ -6,60 +6,144 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
+import { makeStyles } from "@mui/styles";
+import { Drawer, useTheme } from "@mui/material";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 const Navigation = () => {
   const { user, logOut } = useAuth();
 
-  const styles = {
-    textDecoration: "none",
-    color: "white",
-  };
+  const theme = useTheme();
+  const useStyle = makeStyles({
+    navItem: {
+      textDecoration: "none",
+      color: "#fff",
+    },
+    navIcon: {
+      [theme.breakpoints.up("sm")]: {
+        display: "none !important",
+      },
+    },
+    navItemContainer: {
+      [theme.breakpoints.down("sm")]: {
+        display: "none !important",
+      },
+    },
+    navLogo: {
+      [theme.breakpoints.down("sm")]: {
+        textAlign: "right",
+      },
+    },
+    mobileNavItem: {
+      textDecoration: "none",
+      color: "#000",
+    },
+  });
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+  const { navItem, navIcon, navItemContainer, navLogo, mobileNavItem } = useStyle();
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img
-              src="https://global.yamaha-motor.com/shared/img/rwd_identity.png"
-              alt=""
-            />
-          </Typography>
-          <NavLink style={styles} to="/">
-            <Button color="inherit">Products</Button>
-          </NavLink>
-          <NavLink style={styles} to="/explore">
-            <Button color="inherit">Explore</Button>
-          </NavLink>
-          <NavLink style={styles} to="/dashboard">
-            <Button color="inherit">Dashboard</Button>
-          </NavLink>
+  const [state, setState] = React.useState(false);
+
+  const list = (
+    <Box sx={{ width: 250 }} role="presentation">
+      <List>
+        <ListItem button>
+          <ListItemText>
+            <Link className={mobileNavItem} to="/">
+              PRODUCTS
+            </Link>
+          </ListItemText>
+        </ListItem>
+        <ListItem button>
+          <Link className={mobileNavItem} to="/explore">
+            EXPLORE
+          </Link>
+        </ListItem>
+        <ListItem button>
+          <Link className={mobileNavItem} to="/dashboard">
+            DASHBOARD
+          </Link>
+        </ListItem>
+        <ListItem button>
           {user?.email ? (
-            <Box>
-              <Button onClick={logOut} color="inherit">
-                Logout
-              </Button>
-            </Box>
+            <Button onClick={logOut} color="inherit">
+              Logout
+            </Button>
           ) : (
-            <NavLink style={styles} to="/login">
+            <NavLink className={mobileNavItem} to="/login">
               <Button color="inherit">Login</Button>
             </NavLink>
           )}
-        </Toolbar>
-      </AppBar>
+        </ListItem>
+      </List>
+      <Divider />
     </Box>
+  );
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              className={navIcon}
+              onClick={() => setState(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography
+              className={navLogo}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <img
+                src="https://global.yamaha-motor.com/shared/img/rwd_identity.png"
+                alt=""
+              />
+            </Typography>
+            <Box className={navItemContainer}>
+              <NavLink className={navItem} to="/">
+                <Button color="inherit">Products</Button>
+              </NavLink>
+              <NavLink className={navItem} to="/explore">
+                <Button color="inherit">Explore</Button>
+              </NavLink>
+              <NavLink className={navItem} to="/dashboard">
+                <Button color="inherit">Dashboard</Button>
+              </NavLink>
+              {user?.email ? (
+                <Button onClick={logOut} color="inherit">
+                  Logout
+                </Button>
+              ) : (
+                <NavLink className={navItem} to="/login">
+                  <Button color="inherit">Login</Button>
+                </NavLink>
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div>
+        <React.Fragment>
+          <Drawer open={state} onClose={() => setState(false)}>
+            {list}
+          </Drawer>
+        </React.Fragment>
+      </div>
+    </>
   );
 };
 
